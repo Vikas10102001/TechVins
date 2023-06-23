ffe54dcfc38da5f3b975c47b18d165486708e309
Vikas <provikas2001@gmail.com>
2023-06-23 23:34:54 +0530

    added routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Signup from './pages/auth/Signup';
+import Login from './pages/auth/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
   );
 }
 
