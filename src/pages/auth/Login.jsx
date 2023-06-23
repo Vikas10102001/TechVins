17f2826b86e5324435d12148fb7b0b0d96c38363
Vikas <provikas2001@gmail.com>
2023-06-24 02:01:30 +0530

    updated Login and signup pages

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import AppLayout from '../../component/AppLayout'
+import AuthWrapper from '../../component/AuthWrapper'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <AppLayout>
+      <AuthWrapper title="Create your account!">
+        <form>
+          <div className="input-block">
+            <label htmlFor="email" className="input-label">
+              Email
+            </label>
+            <input
+              type="email"
+              autoComplete="off"
+              name="email"
+              id="email"
+              // value={values.email}
+              // onChange={handleChange}
+              // onBlur={handleBlur}
+            />
+            {/* {errors.email && touched.email ? (
+              <p className="form-error">{errors.email}</p>
+            ) : null} */}
+          </div>
+          <div className="input-block">
+            <label htmlFor="password" className="input-label">
+              Password
+            </label>
+            <input
+              type="password"
+              autoComplete="off"
+              name="password"
+              id="password"
+              // value={values.password}
+              // onChange={handleChange}
+              // onBlur={handleBlur}
+            />
+            {/* {errors.password && touched.password ? (
+              <p className="form-error">{errors.password}</p>
+            ) : null} */}
+          </div>
+            <button className="input-button" type="submit">
+              Login
+            </button>
+        </form>
+      </AuthWrapper>
+    </AppLayout>
   )
 }
