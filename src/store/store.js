e2a5add5b7ea28628cbd8b2777365f66bb0e5c26
Vikas <provikas2001@gmail.com>
2023-06-24 23:10:43 +0530

    connected auth  slice with store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import alertSlice from "./slice/alert";
+import authSlice from "./slice/auth";
 const store = configureStore({
   reducer: {
     alert: alertSlice.reducer,
-  }
+    auth: authSlice.reducer,
+  },
 });
 export default store;
