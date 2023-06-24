109a2ff33f573437ea05d71cac2f8421777014d4
Vikas <provikas2001@gmail.com>
2023-06-24 23:12:10 +0530

    auth handling functions

---
+++ src/utils/shared/auth.js
@@ -0,0 +1,48 @@
+import alertSlice from "../../store/slice/alert";
+import store from "../../store/store";
+
+//function for handling auth
+export const authFunction = (authType, values) => {
+  console.log(values);
+  try {
+    //Send api request for authentication
+    if (authType === "SIGNUP") {
+      //send request to endpoint for signup
+    } else if ((authType = "LOGIN")) {
+      //send request to endpoint for login
+    }
+
+    //saving auth data like token sent from server in localstorage
+    //or showing error if any occured
+    let error = false;
+    if (error) {
+      store.dispatch(
+        alertSlice.actions.setAlert({ type: "error", content: "error" }) //not working
+      );
+    } else if (!error) {
+      localStorage.setItem("auth", 1);
+      store.dispatch(
+        alertSlice.actions.setAlert({
+          type: "success",
+          content:
+            authType === "SIGNUP" ? "Signup successful" : "Login successful",
+        })
+      );
+      window.location.pathname = "/";
+    }
+  } catch (err) {
+    console.log(err);
+    store.dispatch(
+      alertSlice.actions.setAlert({
+        type: "error",
+        content: err?.message ? err.message : "Something went wrong",
+      })
+    );
+  }
+};
+
+//logging out
+export const signout = () => {
+  window.location.pathname = "/login";
+  localStorage.removeItem("auth");
+};
