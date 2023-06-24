8bcbdf0fd07040028f57d17e0929de597b3b00fb
Vikas <provikas2001@gmail.com>
2023-06-24 23:14:02 +0530

    logging out user if no auth detail is present in local storage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppLayout from '../component/AppLayout'
 
 export default function Home() {
+
+  useEffect(()=>{
+    const auth=localStorage.getItem('auth')
+    if(!auth)
+    {
+      window.location.pathname = "/";
+    }
+  },[])
   return (
     <AppLayout>
       <h1 className='home'>
