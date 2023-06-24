20e06cae70200470356c049872a273f8ac036da2
Vikas <provikas2001@gmail.com>
2023-06-24 18:18:21 +0530

    added alert slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import alertSlice from "./slice/alert";
 const store = configureStore({
   reducer: {
-   
+    alert: alertSlice.reducer,
   }
 });
 export default store;
