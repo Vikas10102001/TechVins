919556bc13fab41ead9bfde5bd8040279b0d84fe
Vikas <provikas2001@gmail.com>
2023-06-25 00:02:44 +0530

    updated index.js

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 
+//form validation schemas
+
 export const signupSchema = Yup.object({
   name: Yup.string().min(3).max(30).required("Please enter your name"),
   email: Yup.string().email().required("Please enter your email"),
@@ -18,6 +20,6 @@ export const loginSchema = Yup.object({
   email: Yup.string().email().required("Please enter your email"),
   password: Yup.string().matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/,
-    "Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long"
-  ),
+    "Invalid password!"         //no need for displaying whole error message while login
+  ).required("Password is required"),
 });
