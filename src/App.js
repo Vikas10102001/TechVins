494885df85cdd5ff3ff7e9e9707fcf0f620bdb20
Vikas <provikas2001@gmail.com>
2023-06-24 18:20:32 +0530

    rendering alert conditionally

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Signup from './pages/auth/Signup';
-import Login from './pages/auth/Login';
-import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
+import Home from "./pages/Home";
+import Alert from "./utils/shared/component/Alert";
+import { useSelector } from "react-redux";
 
 function App() {
+  const alertData = useSelector((state) => {
+    return state.alert.data;
+  });
+  console.log(alertData);
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {alertData && <Alert data={alertData} />}
+    </>
   );
 }
 
