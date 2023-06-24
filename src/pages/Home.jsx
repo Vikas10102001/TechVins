47c92cbbbced28de0452d345a43c579bde08c2f2
Vikas <provikas2001@gmail.com>
2023-06-25 00:28:54 +0530

    updated Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 import AppLayout from "../component/AppLayout";
 
 export default function Home() {
+  const auth = localStorage.getItem("auth");
   useEffect(() => {
     //logging out user if no auth data(token) is present in local storage
-    const auth = localStorage.getItem("auth");
     if (!auth) {
       window.location.pathname = "/signup"; //better to redirect to login but as per assignment requirements given priority to signup
     }
-  }, []);
+  }, [auth]);
   return (
     <AppLayout>
       <h1 className="home">Hello! Welcome Back</h1>
